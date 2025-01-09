eae3b97c8f9cc297bc741bac3641bfa8725a6065
Jobit Joseph <51044706+jobitjoseph@users.noreply.github.com>
2025-01-09 14:56:46 +0530

    Update udpdriver.py

--- cflib/crtp/udpdriver.py
+++ cflib/crtp/udpdriver.py
@@ -70,12 +70,12 @@ class UdpDriver(CRTPDriver):
         self.socket.setblocking(False)
         self.addr = ('192.168.43.42', 2390)
         try:
-            self.socket.bind(('', 0))  # Dynamic port for local binding
+            self.socket.bind(('', 2399))  # Dynamic port for local binding
             print(f"Socket bound to: {self.socket.getsockname()}")
             self.socket.connect(self.addr)
             print(f"Socket connected to: {self.addr}")
             str1 = b'\xFF\x01\x01\x01'
-            self.socket.send(str1)
+            self.socket.send(str1,self.addr)
             print("Connection signal sent.")
         except OSError as e:
             print(f"Socket connection error: {e}")
